47a58b5d244cb9a435a1ca8f47fc9e6c902eb905
IGOR-67 <ya.m67@yandex.ru>
2026-08-20 22:05:32 +0300

    Add project dependencies (langchain, langchain-community, etc.)

--- .venv/main.py
+++ .venv/main.py
@@ -1,2 +1,10 @@
-# test
-print("Hello World")
+from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+
+load_dotenv()
+
+model = ChatOpenAI(model="gpt-4.1-mini")
+response = model.invoke("Привет как дела?")
+print(response.content)
+
+
